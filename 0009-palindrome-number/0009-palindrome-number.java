b31a52f0f7ec08d62672e48adaf3fdc289ac37e5
Gautam2001j <111883324+Gautam2001j@users.noreply.github.com>
2024-08-06 23:10:55 +0530

    Time: 5 ms (71.15%), Space: 43.9 MB (42.33%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        
+        if(x<0){
+            return false;
+        }
+        int count =0;
+        int temp = x;
+        while(temp != 0){
+            int a = temp%10;
+            count = count*10+a;
+            temp /= 10;
+        }
+        return (count == x);
     }
 }
